35dd644824762a4d61df4d8cff5af064792b3ee1
Twan <twanhelleman@gmail.com>
2019-10-01 09:53:31 +0200

    MainMenu and LevelSelect
    main menu added
    level select add
    still need to test

---
+++ Assets/Script/Twan/LevelSelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    [SerializeField]
+    int level;
+    int reachedLevel;
+    void Start()
+    {
+      reachedLevel =  FindObjectOfType<InformationHolder>().levelReached;
+        if (level > reachedLevel)
+        {
+            GetComponent<Button>().interactable = false;
+        }
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Level" + level);
+    }
+}
