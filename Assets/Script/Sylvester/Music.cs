26d3280b475b29a02e44ee65cabef07779fed453
SylvesterMolenaar <sylvestervmolenaar@gmail.com>
2019-10-04 15:06:10 +0200

    Music
    added music to menu and game

---
+++ Assets/Script/Sylvester/Music.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music : MonoBehaviour
+{
+    private AudioSource _AudioSource;
+    [SerializeField] private AudioClip _MenuMusic;
+    [SerializeField] private AudioClip _InGameMusic;
+    private Scene _CurrentScene;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _AudioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        _CurrentScene = SceneManager.GetActiveScene();
+
+        if (_CurrentScene.name == "Main Menu" && _AudioSource.clip.name != "MenuMusic")
+        {
+            _AudioSource.clip = _MenuMusic;
+            PlayMusic();
+        }
+        else if (_CurrentScene.name == "LevelSelect(Test)" && _AudioSource.clip.name != "MenuMusic")
+        {
+            _AudioSource.clip = _MenuMusic;
+            PlayMusic();
+        }
+        else if (_CurrentScene.name == "Level1" || _CurrentScene.name == "Level2" || _CurrentScene.name == "Level3" || _CurrentScene.name == "Level4")
+        {
+            if (_AudioSource.clip.name != "InGameMusic")
+            {
+                _AudioSource.clip = _InGameMusic;
+                PlayMusic();
+            }
+        }
+    }
+
+    public void PlayMusic()
+    {
+        if (_AudioSource.isPlaying) return;
+        _AudioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _AudioSource.Stop();
+    }
+
+}
