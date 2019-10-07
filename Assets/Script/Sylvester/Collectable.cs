c3c9dcb04197dc8ebd3e420a93effd068cf91cf1
SylvesterMolenaar <sylvestervmolenaar@gmail.com>
2019-10-07 13:54:01 +0200

    Collectable
    small changes, nothing to note specifically

--- Assets/Script/Sylvester/Collectable.cs
+++ Assets/Script/Sylvester/Collectable.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    [SerializeField] private Transform _Portal;
+    [SerializeField] private GameObject _EndPortal;
     [SerializeField] private AudioSource _AudioSource;
     [SerializeField] private AudioClip _CollectedSound;
     [SerializeField] private SpriteRenderer _SpriteRenderer;
     private bool _Collected = false;
     private bool _SoundPlayed = false;
 
+    private void Start()
+    {
+        _EndPortal = GameObject.Find("EndPortal");
+        _AudioSource = GameObject.Find("GameManager").GetComponent<AudioSource>();
+        _SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        _EndPortal.GetComponent<SpriteRenderer>().enabled = false;
+        _EndPortal.GetComponent<Collider2D>().enabled = false;
+        _EndPortal.GetComponent<ParticleSystem>().Stop();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _Portal.transform.gameObject.SetActive(true);
+        _EndPortal.GetComponent<SpriteRenderer>().enabled = true;
+        _EndPortal.GetComponent<Collider2D>().enabled = true;
+        _EndPortal.GetComponent<ParticleSystem>().Play();
         _AudioSource.clip = _CollectedSound;
         _AudioSource.Play();
         _SoundPlayed = true;
