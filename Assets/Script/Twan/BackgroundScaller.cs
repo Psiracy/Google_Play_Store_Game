c9a221ca4e5b6036dd972598a52c697780c9c7b1
Twan <twanhelleman@gmail.com>
2019-11-13 00:41:05 +0100

    bug fixes added new art and levels
    -added the corner pieces
    -added the new hinge art
    -added the new background in the main menu
    -changed the way the player singelton works
    -made the music an singleton
    -let's the menu reset when you delete your save file
    -made an background fitter
    -added new buttons and a scroll rect

---
+++ Assets/Script/Twan/BackgroundScaller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScaller : MonoBehaviour
+{
+    Camera cam;
+    float offset = .022f;
+
+    void Start()
+    {
+        cam = Camera.main;
+        transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float size = (cam.orthographicSize / 10) + offset;
+        transform.localScale = new Vector3(size, size, size);
+    }
+}
